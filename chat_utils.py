88abe0a80e7a09c04fc34358d3eaa64fa37af16a
Naaamsukh <naaamsukh@gmail.com>
2024-06-18 21:41:17 +0530

    feat: add streaming and parallel output generation

--- chat_utils.py
+++ chat_utils.py
@@ -1,14 +1,16 @@
 import os
 
+import asyncio
 import streamlit as st
 from llama_index.llms.openai import OpenAI
+from openai import AsyncOpenAI
 
 from constants import SHORT_BOOk_GENERATOR
 
 openai_api_key = os.environ.get("OPENAI_API_KEY")
 
 llm = OpenAI(api_key=openai_api_key, model="gpt-4o")
-
+llm_async = AsyncOpenAI(api_key=openai_api_key)
 
 def generate_rag_response(query):
     """
@@ -36,3 +38,30 @@ def generate_rag_response(query):
 def chat(prompt):
     response = llm.complete(prompt)
     return response.text
+
+
+async def streamchat(placeholder,query):
+    if "query_engine" not in st.session_state:
+        return "Sorry no document found"
+
+    chunks = st.session_state.query_engine.query(query)
+
+    context_str = "\n\n".join(chunk.get("content") for chunk in chunks)
+    prompt = SHORT_BOOk_GENERATOR.format(
+        subtopic_summary=query,context=context_str
+    )
+    stream_coroutine  = llm_async.chat.completions.create(
+        model="gpt-4-turbo",
+        messages=[
+            {"role": "system", "content": "You are a helpful assistant."},
+            {"role": "user", "content": prompt},
+        ],
+        stream=True
+    )
+    stream = await stream_coroutine
+    streamed_text = "# "
+    async for chunk in stream:
+        chunk_content = chunk.choices[0].delta.content
+        if chunk_content is not None:
+            streamed_text = streamed_text + chunk_content
+            placeholder.info(streamed_text)
--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 import streamlit as st
 from dotenv import load_dotenv
 from pdfminer.high_level import extract_text
-
-from chat_utils import generate_rag_response
+import asyncio
+from chat_utils import generate_rag_response,streamchat
 from file_utils import generate_document
 from query_engine import get_query_engine_from_text
 
 load_dotenv()
 
-def main():
+async def main():
     st.title("Infi Book AI")
     uploaded_files = st.file_uploader("Upload File",
                                       accept_multiple_files=True,
@@ -17,12 +17,12 @@ def main():
 
     if uploaded_files:
         print("Inside uploaded files")
+        all_text = ""
         for uploaded_file in uploaded_files:
-            text = extract_text(uploaded_file)
-            query_engine = get_query_engine_from_text(text, top_k=7)
-
-            if "query_engine" not in st.session_state:
-                st.session_state.query_engine = query_engine
+            all_text += extract_text(uploaded_file) + "\n\n"
+        
+        query_engine = get_query_engine_from_text(all_text, top_k=7)
+        st.session_state.query_engine = query_engine
 
     text_area = st.text_area("Provide sub chapters seperated by | ")
     textsplit = ""
@@ -33,16 +33,25 @@ def main():
 
     if st.button('Generate documents'):
         print("Generate documents called")
-        for idx, text in enumerate(textsplit, start=1):
-            response = generate_rag_response(text)
+        num_columns = len(textsplit)
+        columns = st.columns(num_columns)
+        tasks = []
 
-            download_link = generate_document(response, idx)
-            generated_documents.append(download_link)
+        for idx, (text, col) in enumerate(zip(textsplit, columns), start=1):
+            output_placeholder = col.empty()
+            # Add each streamchat task to our list
+            tasks.append(streamchat(output_placeholder, text.strip()))
+
+        # Run all tasks concurrently
+        await asyncio.gather(*tasks)
+
+            # download_link = generate_document(response, idx)
+            # generated_documents.append(download_link)
             
             
-    for document in generated_documents:
-        st.markdown(document, unsafe_allow_html=True)
+    # for document in generated_documents:
+    #     st.markdown(document, unsafe_allow_html=True)
 
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
